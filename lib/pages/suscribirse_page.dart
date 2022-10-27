16de414e9b7b240a7714218184c819ff6fd1d4a0
Abraham Raymundo <70590661+Pimpollo30@users.noreply.github.com>
2022-10-27 18:51:52 -0500

    SuscribirsePage modified

--- lib/pages/suscribirse_page.dart
+++ lib/pages/suscribirse_page.dart
@@ -6,7 +6,56 @@ class SuscribirsePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Hola"),
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      width: double.infinity,
+      child: Form(
+        child: Column(
+          children: [
+            SizedBox(height:10),
+            Text(
+              "Recibe información, acontecimientos,\nofertas o novedades acerca de las\ninstituciones de la entidad",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize:20,
+                fontWeight: FontWeight.bold,
+              )
+              ),
+            SizedBox(height:10),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Correo electrónico',
+                hintText: 'Correo electrónico'
+              ),
+            ),
+            SizedBox(height:10),
+            DropdownButtonFormField(
+              items: [DropdownMenuItem(child: Text('Ciudad'))], 
+              onChanged: (value) {}),
+            SizedBox(height:10),
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 15,
+                ),
+                child: Text(
+                  "Suscribirse",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                    ),
+                  ),
+              ),
+              color: Colors.blue,
+              onPressed: () {}
+              )
+          ],
+        ),
+      ),
     );
   }
 }
