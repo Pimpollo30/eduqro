f95e7d66d2a178c3f712b3ad1b1b4030e07c442b
Abraham Raymundo <70590661+Pimpollo30@users.noreply.github.com>
2022-10-28 01:12:57 -0500

    InstitucionesPage added

---
+++ lib/pages/instituciones_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InstitucionesPage extends StatelessWidget {
+  const InstitucionesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text("Instituciones")),
+    );
+  }
+}
