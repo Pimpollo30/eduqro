e0b4efaec07dca0ae085e954a3ee7959154b9dcd
Abraham Raymundo <70590661+Pimpollo30@users.noreply.github.com>
2022-10-29 23:54:53 -0500

    Main modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:eduqro/pages/agregar_institucion_page.dart';
+import 'package:eduqro/pages/crear_newsletter_page.dart';
+import 'package:eduqro/pages/editar_newsletter_page.dart';
 import 'package:eduqro/pages/home_page.dart';
+import 'package:eduqro/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,6 +22,9 @@ class MyApp extends StatelessWidget {
       routes: {
         'home': (_) => HomePage(),
         'agregarInst': (_) => AgregarInstitucionPage(), 
+        'crearNews': (_) => CrearNewsletterPage(),
+        'editarNews': (_) => EditarNewsletterPage(),
+        'login': (_) => LoginPage(),
       },
     );
   }
