9352219e13faa056875401a4e065914574a6f2f6
Abraham Raymundo <70590661+Pimpollo30@users.noreply.github.com>
2022-10-29 23:54:21 -0500

    NewsletterPage Modified

--- lib/pages/newsletter_page.dart
+++ lib/pages/newsletter_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/newsletter_card.dart';
+
 class NewsletterPage extends StatelessWidget {
   const NewsletterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var estilo = TextStyle(fontWeight: FontWeight.w500, fontSize: 20, height: 0);
+     return Container(
+       child: Stack(
+         children: [
+            ListView.builder(
+              itemCount: 2,
+              itemBuilder: (BuildContext context, int index) {
+                return NewsletterCard();
+              },
+            ),
+            Positioned(
+            right:10,
+            bottom: 10,
+            child: FloatingActionButton(heroTag:"crearNews",child:Icon(Icons.add),onPressed: () {
+              Navigator.pushNamed(context, "crearNews");
+            },),
+          ),
+         ],
+       ),
+     );
   }
 }
